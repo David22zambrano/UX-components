23f7676c05be7dab5011e52b875d6e19a5f96ab2
David Sebastian <David Sebastian Pea Zambrano>
2024-04-03 11:29:06 -0500

    Correcion mapeo pagina

--- UX componentes AG/ux-components/src/app/components/menu/menu-principal.component.ts
+++ UX componentes AG/ux-components/src/app/components/menu/menu-principal.component.ts
@@ -3,6 +3,36 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-menu-principal',
   templateUrl: './menu-principal.component.html',
-  styleUrl: './menu-principal.component.scss'
+  styleUrls: ['./menu-principal.component.scss']
 })
-export class MenuPrincipalComponent { }
+export class MenuPrincipalComponent {
+  showComponent: any = false; 
+    menuItems = [
+      { name: 'COMPONENTES', route: '/' },
+      { name: 'Autocomplete', route: '/autocomplete' },
+      { name: 'Button', route: '/button' },
+      { name: 'Icon Button', route: '/icon' },
+      { name: 'Floating Action Button', route: '/fab' },
+      { name: 'Button group', route: '/buttonGroup' },
+      { name: 'Checkbox', route: '/checkbox' },
+      { name: 'Radio - Radio Group', route: '/radio' },
+      { name: 'Rating', route: '/rating' },
+      { name: 'Select', route: '/select' },
+      { name: 'Slider', route: '/slider' },
+      { name: 'SwitchFormControl', route: '/switchFormControl' },
+      { name: 'Switch', route: '/switch' },
+      { name: 'Badge', route: '/badge' },
+      { name: 'Text Field', route: '/textfield' },
+      { name: 'Toggle Button', route: '/toggleButton' },
+      { name: 'Avatar', route: '/avatar' },
+      { name: 'Chip', route: '/chip' },
+      { name: 'Icon', route: '/icon' },
+      { name: 'List', route: '/list' },
+      { name: 'Tooltip', route: '/tooltip' },
+      { name: 'Alert', route: '/alert' },
+      { name: 'Progress', route: '/progress' },
+      { name: 'Table', route: '/table' },
+      { name: 'Accordion', route: '/accordion' }
+  ];
+}
+
