4b3f710fb576b7151722013c8a4698e4aee7f313
david.pena <David.zambrano17@outlook.com>
2024-05-07 14:40:39 -0500

    Qa ajuste final

--- UX componentes AG/ux-components/src/app/components/list-components/table/table.component.ts
+++ UX componentes AG/ux-components/src/app/components/list-components/table/table.component.ts
@@ -2,42 +2,44 @@ import { Component } from '@angular/core';
 import { SelectionModel } from '@angular/cdk/collections';
 import { MatTableDataSource } from '@angular/material/table';
 
-export interface TableCell {
+
+export interface TableData {
   columnA: string;
   columnB: string;
   columnC: string;
   columnD: string;
 }
 
-const ELEMENT_DATA: TableCell[] = Array(4).fill({ columnA: 'head', columnB: 'cell', columnC: 'cell', columnD: 'cell' });
+const ELEMENT_DATA: TableData[] = [
+  {columnA: "Cell", columnB: 'Cell', columnC: "Cell", columnD: 'Cell'},
+  {columnA: "Cell", columnB: 'Cell', columnC: "Cell", columnD: 'Cell'},
+  {columnA: "Cell", columnB: 'Cell', columnC: "Cell", columnD: 'Cell'},
+  {columnA: "Cell", columnB: 'Cell', columnC: "Cell", columnD: 'Cell'},
+];
 
 @Component({
   selector: 'app-table',
   templateUrl: './table.component.html',
-  styleUrl: './table.component.scss'
+  styleUrls: ['./table.component.scss']
 })
 export class TableComponent {
   displayedColumns: string[] = ['select', 'columnA', 'columnB', 'columnC', 'columnD'];
-  dataSource = new MatTableDataSource<TableCell>(ELEMENT_DATA);
-  selection = new SelectionModel<TableCell>(true, []);
+  dataSource = new MatTableDataSource<TableData>(ELEMENT_DATA);
+  selection = new SelectionModel<TableData>(true, []);
 
   isAllSelected() {
     const numSelected = this.selection.selected.length;
     const numRows = this.dataSource.data.length;
     return numSelected === numRows;
   }
-  toggleAllRows() {
-    if (this.isAllSelected()) {
-      this.selection.clear();
-      return;
-    }
 
-    this.selection.select(...this.dataSource.data);
+  masterToggle() {
+    this.isAllSelected() ?
+        this.selection.clear() :
+        this.dataSource.data.forEach(row => this.selection.select(row));
   }
-  checkboxLabel(row?: TableCell): string {
-    if (!row) {
-      return `${this.isAllSelected() ? 'deselect' : 'select'} all`;
-    }
-    return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.columnA + 1}`;
+
+  logSelection() {
+    this.selection.selected.forEach(s => console.log(s.columnB));
   }
 }
